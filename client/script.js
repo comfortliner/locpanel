26364cde33bedc4f8471182dbd43070ef07d956b
comfortliner <andreas_werz@gmx.de>
2024-04-17 14:05:53 +0200

    smale style changes, added jquery files

--- client/script.js
+++ client/script.js
@@ -25,7 +25,7 @@ const getSelectedDoW = () => {
 };
 
 // eslint-disable-next-line no-undef
-const socket = io.connect({'path': socketioServerPath});
+const socket = io.connect({ path: socketioServerPath });
 
 const sendAction = (action, data) => {
   const defaultKeyValue = {
